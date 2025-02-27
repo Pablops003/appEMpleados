52856a2bb6e0ae574e6ef976e7374f7a04839bfb
Pablops003 <pabpeiser@alu.edu.gva.es>
2025-02-27 13:41:56 +0100

    Acabado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'appEMpleados' title`, () => {
+  it(`should have the 'appEMpleados' titulo`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('appEMpleados');
+    expect(app.titulo).toEqual('appEMpleados');
   });
 
-  it('should render title', () => {
+  it('should render titulo', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, appEMpleados');
   });
+  
+  // Acabado
 });
